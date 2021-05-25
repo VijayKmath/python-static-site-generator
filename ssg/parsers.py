620a32f16d27dd8c6bf2d698e54c191a8efe55c2
Vijayamahantesh Karpurmath <vijay.karpurmath8163@gmail.com>
2021-05-25 12:02:09 +0530

    Module-2: 4th Commit

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,36 +3,29 @@ from pathlib import Path
 import shutil
 
 
-class Parser:
-    extensions = []
-    extensions = List[str]
+class Parser:    
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
-        if extension in self.extensions:
-            return True
-        
-        return False
+        return extension in self.extensions
     
     def parser(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
     def read(self, path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
         full_path =self.dest / path.with_suffix(ext).name
-        with open(full_path) as file:
+        with open(full_path, "w") as file:
             file.write(content)
     
     def copy(self, path, source, dest):
-        dest2 = dest / path.relative_to(source)
-        shutil.copy2(path, dest2)
+        shutil.copy2(path, dest / path.relative_to(source))
 
     class ResourceParser:
         extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
         def parse(self, path: Path, source: Path, dest: Path):
-            copy(self, path, source, dest)
-
-
+            self.copy(self, path, source, dest)
