48d7f01ca2287c5ff02575c00e956f37e9d860c5
Vijayamahantesh Karpurmath <vijay.karpurmath8163@gmail.com>
2021-05-25 11:50:50 +0530

    Module-2: 2nd Commit

--- ssg/site.py
+++ ssg/site.py
@@ -9,17 +9,17 @@ class Site:
         self.parsers = []
 
     def create_dir(self, path):
-        directory = self.dest / Path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok= True)
     
     def build(self):
         self.dest.mkdir(parents=True, exist_ok= True)
 
-        for path in self.source.rglob("*"):
+        for path in source.rglob("*"):
             if Path.is_dir(path):
                 self.create_dir(path)
             elif Path.is_file(path):
-                run_parser(path)    
+                self.run_parser(path)    
     
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -27,7 +27,7 @@ class Site:
                 return parser
     
     def run_parser(self, path):
-        parser = load_parser(Path.suffix(path))
+        parser = self.load_parser(Path.suffix(path))
         if not parser == None:
             parser.parse(path, source, dest)
         else:
