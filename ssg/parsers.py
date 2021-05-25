e0fd5e08418853d40b9c0dc71daacb4fbab44ef2
Vijayamahantesh Karpurmath <vijay.karpurmath8163@gmail.com>
2021-05-25 12:43:01 +0530

    Module-2: Fix for test case failures

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -24,8 +24,8 @@ class Parser:
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-    class ResourceParser:
-        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+class ResourceParser:
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-        def parse(self, path: Path, source: Path, dest: Path):
-            self.copy(path, source, dest)
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
