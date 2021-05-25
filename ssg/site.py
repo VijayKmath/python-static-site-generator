b168638f70c8f4edbc7ba7f13e94aadb84fc31fc
Vijayamahantesh Karpurmath <vijay.karpurmath8163@gmail.com>
2021-05-25 11:44:03 +0530

    Module 2 changes

--- ssg/site.py
+++ ssg/site.py
@@ -3,17 +3,33 @@ import os
 
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = []
 
     def create_dir(self, path):
-        directory = self.dest / path.relative_to(self.source)
+        directory = self.dest / Path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok= True)
     
     def build(self):
         self.dest.mkdir(parents=True, exist_ok= True)
 
         for path in self.source.rglob("*"):
-            if path.is_dir(path):
+            if Path.is_dir(path):
                 self.create_dir(path)
+            elif Path.is_file(path):
+                run_parser(path)    
+    
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extesion(extension):
+                return parser
+    
+    def run_parser(self, path):
+        parser = load_parser(Path.suffix(path))
+        if not parser == None:
+            parser.parse(path, source, dest)
+        else:
+            print("Not implemented.")
+        
